d84236ddd52f01a47c6e0975ee8cd1b6baa8a287
corginomics <anvle@ucdavis.edu>
2016-08-16 23:07:12 +0000

    Implement code that passes unit tests

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,18 +1,18 @@
+from functools import reduce
+
 
 def add(*args):
-    # your implementation here
-    pass
+    return reduce(lambda x, y: x + y, args)
+
 
 def subtract(*args):
-    # your implementation here
-    pass
+    return reduce(lambda x, y: x - y, args)
+    
 
 def multiply(*args):
-    # your implementation here
-    pass
+    return reduce(lambda x, y: x * y, args)
+
 
 def divide(*args):
-    # your implementation here
-    pass
-
-# add your custom operations here
+    return reduce(lambda x, y: x / y, args)
+    
